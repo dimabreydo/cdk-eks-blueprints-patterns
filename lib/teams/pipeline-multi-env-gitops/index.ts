b012edec082efbae98db76795aa87e418318d8c4
Dima Breydo <dimabr@amazon.com>
2023-08-27 22:20:21 +0300

    remove team

--- lib/teams/pipeline-multi-env-gitops/index.ts
+++ lib/teams/pipeline-multi-env-gitops/index.ts
@@ -1,4 +1,4 @@
-export { BackendCrystalTeam } from './team-backend-crystal';
+// export { BackendCrystalTeam } from './team-backend-crystal';
 export { FrontendTeam } from './team-backend-frontend';
 export { BackendNodejsTeam } from './team-backend-nodejs';
 export { CorePlatformTeam } from './team-platform';
